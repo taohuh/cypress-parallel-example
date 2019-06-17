142edcbbfe24e24ee9a5d3d527f5c0fa75a6fd82
taohuh <pimpakarn@maqe.com>
2019-06-17 12:53:24 +0700

    Change test title

--- cypress/integration/sample4.spec.js
+++ cypress/integration/sample4.spec.js
@@ -1,5 +1,5 @@
 describe('Cypress parallel run example - 4', () => {
-    it('Should display the blog link RSS', () => {
+    it('Should display the blog link', () => {
         cy.visit('https://mherman.org');
         cy.get('a').contains('RSS');
     });
